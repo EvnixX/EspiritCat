3d392a9f5f3abb8cf9a4c84189df03e4c552820e
evnixx <evelynvmontano97@gmail.com>
2022-08-08 23:11:08 -0500

    fin 8/08 11:11

---
+++ Espirintcat/Assets/Scripts/ScriptsGame/Esconderse.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esconderse : MonoBehaviour
+{
+    [SerializeField]
+    private Transform dentro, fuera;
+
+    [SerializeField]
+    private float tiempo;
+
+    private GameObject Player;
+
+    public bool entra;
+    private bool sale;
+
+    Transform playerT;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        playerT = Player.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       if(entra == true)
+        {
+            playerT.position = Vector3.Lerp(playerT.position, dentro.position, tiempo * Time.deltaTime);
+            playerT.rotation = Quaternion.Lerp(playerT.rotation, dentro.rotation, tiempo * Time.deltaTime);
+
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                entra = false;
+                sale = true;
+            }
+        }
+
+       if(sale == true)
+        {
+
+            playerT.position = Vector3.Lerp(playerT.position, fuera.position, tiempo * Time.deltaTime);
+            playerT.rotation = Quaternion.Lerp(playerT.rotation, fuera.rotation, tiempo * Time.deltaTime);
+            StartCoroutine(finEscondite());
+        }
+
+
+    }
+
+    IEnumerator finEscondite()
+    {
+        yield return new WaitForSeconds(2);
+        sale = false;
+    }
+}
---
+++ Espirintcat/Assets/Scripts/ScriptsGame/Raycast.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    private int rango = 2;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(player.transform.position, player.transform.forward, out hit, rango))
+        {
+           if(hit.collider.GetComponent<Esconderse>() == true)
+            {
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    hit.collider.GetComponent<Esconderse>().entra = true;
+                }
+
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(player.transform.position, player.transform.forward * rango);
+    }
+}
